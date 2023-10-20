85136f66f8127dfe0e8158634829c8951168a12d
diepnh1602 <110804214+diepnh1602@users.noreply.github.com>
2023-10-20 17:59:39 +0700

    ad

---
+++ Assets/ScriptBases/MiniGame.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame : MonoBehaviour
+{
+    public virtual void OnStart()
+    {
+
+    }
+
+    public virtual void OnGameOver(bool isWin)
+    {
+
+    }
+
+}
