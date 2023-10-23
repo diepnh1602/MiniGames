ca62c8c784988239e0aef70d5e38c29f28ac9cf2
tuanvu <87792686+tuanvus@users.noreply.github.com>
2023-10-23 21:51:38 +0700

    mr bullet

---
+++ Assets/_MiniGame/MrBullet/Scripts/Interactables/IInteractables.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MrBullet
+{
+	public interface IInteractables
+	{
+	    public void Interact();
+	}
+}
---
+++ Assets/_MiniGame/MrBullet/Scripts/Tutorial.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace MrBullet
+{
+	public class Tutorial : MonoBehaviour
+	{
+	    [SerializeField] private CanvasGroup canvasGroup;
+	    [SerializeField] private RectTransform finger;
+	
+	
+	    private void Start() 
+	    {
+	        finger.DOAnchorPosX(184, 1f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+	    }
+	
+	    private void Update()
+	    {
+	        if(Input.GetMouseButtonDown(0))
+	        {
+	           StartCoroutine(StartFade());
+	        }
+	    }
+	
+	    private IEnumerator StartFade()
+	    {
+	        float lerp = 0f;
+	        do
+	        {
+	            lerp += Time.deltaTime;
+	            canvasGroup.alpha = Mathf.Lerp(1, 0, lerp);
+	            yield return null;
+	        } while (lerp < 1f);
+	
+	        gameObject.SetActive(false);
+	    }
+	}
+}
